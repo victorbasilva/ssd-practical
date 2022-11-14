1ee0383f5d03c4c8b874cda7511738cde2f29166
victorbasilva <victorbasilva@gmail.com>
2022-11-14 13:51:38 +0000

    Java Script functions

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require('http'), //HTTP server
+      express = require('express'), //Handling HTTP requests & routing
+      fs = require('fs'), //File system functionalities
+      xmlParse = require('xslt-processor').xmlParse, //XML handling
+      xsltProcess = require('xslt-processor').xsltProcess, //XSLT handling
+      router = express(), //Initialize our router
+      server = http.createServer(router); //Initialize our server
+
+router.get('/, function(req, res)' {
+
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    let xml = fs.readFileSync('menu.xml', 'utf8'),
+        xsl = fs.readFileSync('menu.xsl', 'utf8');
+
+        xml = xmlParse(xml),
+        xsl = xmlParse(xsl);
+
+    let html = xsltProcess(xml, xsl);
+
+    res.end(html.toString());
+    
+    });  
+    
+    server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+
+        const addr = server.address();
+        console.log("Server listening at", addr.address + addr.port)
+
+    })
+
+
